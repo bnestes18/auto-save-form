4beb8cfbaaf36c826244c265cd4384776ae4bf17
Brittney Estes <bnestes2014@gmail.com>
2019-12-01 17:14:57 -0500

    Add function to get input value's element id

--- autosave.js
+++ autosave.js
@@ -3,15 +3,36 @@
     "use strict";
 
     let formElems = Array.prototype.slice.call(document.querySelector('form').elements);
+/*
+    This function gets the id or name attribute value from the input field 
+    */
+    let getId = function (field) {
+        if (field.id.length > 0) {
+            return field.id;
+        }
+
+        if (field.name.length > 0) {
+            return field.name;
+        }
+
+        return null;
+    }
 
     /*
     This function saves the user input data locally (localStorage api) 
     */
     let saveUserInfo = function (event) {
+
+        
         // Stop the callback function if the target is not an input/textarea field in the form
         if (!formElems.includes(event.target)) return;     
+
+        // Get an ID for the input field
+        let id = getId(event.target);
+        if (!id) return;
+
         // Otherwise, save the input value to local storage 
-        return localStorage.setItem(event.target.id, event.target.value);
+        return localStorage.setItem(id, event.target.value);
     }
     /*
     This function retrieves the saved user input and sets the saved value as the input value.
@@ -19,9 +40,14 @@
     */
     let persistUserInfo = function () {
         return formElems.forEach(function (element) {
+
+            // Get an ID for the input field
+            let id = getId(element);
+            // If the element has no ID, skip it
+            if (!id) return;
+
             // Get the saved element id
             let savedValue = localStorage.getItem(element.id);
-
             // Skip if element does not have a saved value
             if (!savedValue) return;            
             // Otherwise, set the saved value to the input value
@@ -33,7 +59,14 @@
     */
     let deleteUserInfo = function(event) {
         if (!event.target.closest('#save-me')) return;
+
         return formElems.forEach(function (element) {
+
+            // Get an ID for the input field
+            let id = getId(element);
+            if (!id) return;
+
+            // Clear all data from local storage
             localStorage.removeItem(element.id);
         })      
     }
