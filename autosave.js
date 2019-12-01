3903c31f7e2699657396e8efc661c6231575df87
Brittney Estes <bnestes2014@gmail.com>
2019-12-01 13:52:11 -0500

    Update function to clear data on submit

--- autosave.js
+++ autosave.js
@@ -1,15 +1,17 @@
 ;(function () {
+
+    'use strict'
+
     let formElems = Array.prototype.slice.call(document.querySelector('form').elements);
-    let submitBtn = document.querySelector('button');
 
     /*
     This function saves the user input data locally (localStorage api) 
     */
     let saveUserInfo = function (event) {
-        if (!formElems.includes(event.target)) return;      // Stop the callback function if the target is not an input/textarea field in the form
-        if (event.target.value !== "") {
-            return localStorage.setItem(event.target.id, event.target.value);
-        }
+        // Stop the callback function if the target is not an input/textarea field in the form
+        if (!formElems.includes(event.target)) return;     
+        // Otherwise, save the input value to local storage 
+        return localStorage.setItem(event.target.id, event.target.value);
     }
     /*
     This function retrieves the saved user input and sets the saved value as the input value.
@@ -17,7 +19,12 @@
     */
     let persistUserInfo = function () {
         return formElems.forEach(function (element) {
+            // Get the saved element id
             let savedValue = localStorage.getItem(element.id);
+
+            // Skip if element does not have a saved value
+            if (!savedValue) return;            
+            // Otherwise, set the saved value to the input value
             element.value = savedValue;
         })
     }
@@ -25,17 +32,17 @@
     This function deletes all saved data from local storage and user input fields
     */
     let deleteUserInfo = function(event) {
-        event.preventDefault();
-        return formElems.forEach(function (element) {
+        if (!event.target.closest('#save-me')) return;
+        formElems.forEach(function (element) {
             localStorage.removeItem(element.id);
-        })
+        })      
     }
 
     persistUserInfo();      // Persist user data on load
 
 
-    document.addEventListener('change', saveUserInfo, false);       // Listens for any change on the input fields
-    submitBtn.addEventListener('click', deleteUserInfo, false);     // Listens for click event on subnit button
+    document.addEventListener('input', saveUserInfo, false);       // Listens for all input on text fields
+    document.addEventListener('submit', deleteUserInfo, false);     // Listens for click event on subnit button
 
 })();
 
